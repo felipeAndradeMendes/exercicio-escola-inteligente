515de42412aa9685f0494835c6d79011c8a659cc
Felipe Mendes <felipemadruga@gmail.com>
2023-09-18 18:19:14 -0300

    Conclui requisito 01

--- src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
+++ src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
@@ -5,17 +5,32 @@ import java.util.Scanner;
 public class PessoaEstudante {
 
   /**
-   * Método calcularIdadeEmDias.!
+   * Método calcularIdadeEmDias.
    */
   public static int calcularIdadeEmDias(int anos, int meses, int dias) {
-    System.out.println("TODO: Implementar a função calcular Idade em Dias.");
-    return 0;
+    return (anos * 365) + (meses * 30) + dias;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações da Pessoa Estudante ");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Qual o nome da Pessoa Estudante?");
+    String nome = scanner.next();
+
+    System.out.println("Qual a sua idade em anos, meses e dias?");
+    System.out.println("anos:");
+    int anos = Integer.parseInt(scanner.next());
+
+    System.out.println("meses:");
+    int meses = Integer.parseInt(scanner.next());
+
+    System.out.println("dias:");
+    int dias = Integer.parseInt(scanner.next());
+
+    int idadeEmDias = calcularIdadeEmDias(anos, meses, dias);
+    System.out.println("A idade de " + nome + " é " + idadeEmDias + ".");
   }
 }

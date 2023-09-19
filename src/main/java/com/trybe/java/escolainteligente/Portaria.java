76b35d3fdc211b5adfa1d8fc5f817fbe01273cbd
Felipe Mendes <felipemadruga@gmail.com>
2023-09-19 11:45:13 -0300

    fix - Corrige output requisito 03

--- src/main/java/com/trybe/java/escolainteligente/Portaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Portaria.java
@@ -1,7 +1,10 @@
 package com.trybe.java.escolainteligente;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
+import java.util.Locale;
+
 import java.util.Scanner;
 
 public class Portaria {
@@ -11,6 +14,8 @@ public class Portaria {
    */
   public static void emitirRelatorio(int qtdFundamental1, int qtdFundamental2, int qtdMedio) {
     int total = qtdFundamental1 + qtdFundamental2 + qtdMedio;
+    DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+    DecimalFormat df = new DecimalFormat("#.0#", symbols);
     double percentFund1 = 100.0 * qtdFundamental1 / total;
     double percentFund2 = 100.0 * qtdFundamental2 / total;
     double percentMedio = 100.0 * qtdMedio / total;
@@ -21,9 +26,9 @@ public class Portaria {
     System.out.println("Ensino Médio: " + qtdMedio);
     System.out.println();
     System.out.println("----- Percentual -----");
-    System.out.println("Ensino Fundamental I: " + percentFund1 + "%");
-    System.out.println("Ensino Fundamental II: " + percentFund2 + "%");
-    System.out.println("Ensino Médio: " + percentMedio + "%");
+    System.out.println("Ensino Fundamental I: " + df.format(percentFund1) + "%");
+    System.out.println("Ensino Fundamental II: " + df.format(percentFund2) + "%");
+    System.out.println("Ensino Médio: " + df.format(percentMedio) + "%");
     System.out.println();
     System.out.println("TOTAL: " + total);
 
